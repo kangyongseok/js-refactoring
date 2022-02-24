f0d10c4753a26fc21e2a2c82e5963170cf60b164
강용석 <kangyongsuek@gmail.com>
2022-02-24 21:21:00 +0900

    modify: 적립 포인트 계산 코드 추출하기

--- first-example/index.js
+++ first-example/index.js
@@ -31,6 +31,13 @@ function amountFor(aPerformance) {
   return result
 }
 
+function volumeCreditsFor(perf) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+  return volumeCredits
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -42,10 +49,7 @@ function statement(invoice, plays) {
   }).format;
   
   for (let perf of invoice.performances) {
-    // 포인트 적립
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트 제공
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf)
