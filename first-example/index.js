8ff54698dbd1f19e38395bd328cf4c7202fef40c
강용석 <kangyongsuek@gmail.com>
2022-02-24 22:35:20 +0900

    modify: 가출나간 함수들 제자리 찾아놓기

--- first-example/index.js
+++ first-example/index.js
@@ -4,91 +4,92 @@ const play = require('./data/plays.json')
 
 console.log(statement(invoice, play))
 
-
-function playFor(aPerformance) {
-  return play[aPerformance.playID]
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch(playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
-  }
-
-  return result
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type) 
-    result += Math.floor(aPerformance.audience / 5);
-
-  return result
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency", 
-    currency: "USD",
-    minimumFractionDigits: 2
-  }).format(aNumber / 100);
-}
-
-function totalVolumeCredits() {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf)
-  }
-
-  return result
-}
-
-function totalAmount() {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf)
-  }
-
-  return result
-}
-
 function renderPlainText(data, plays) {
-  let result = `청구내역 (고객명: ${data.customer})\n`;
-  
+  let result = `청구내역 (고객명: ${data.customer})\n`
+
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(totalAmount())}\n`
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`
   return result
+
+  function amountFor(aPerformance) {
+    let result = 0
+    switch (aPerformance.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+    }
+
+    return result
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+    if ('comedy' === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5)
+
+    return result
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100)
+  }
+
+  function totalVolumeCredits() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf)
+    }
+
+    return result
+  }
+
+  function totalAmount() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += amountFor(perf)
+    }
+
+    return result
+  }
 }
 
 function statement(invoice, plays) {
-  const statementData = {};
+  const statementData = {}
   statementData.customer = invoice.customer
-  statementData.performances = invoice.performances.map(enrichPerfomence);
+  statementData.performances = invoice.performances.map(enrichPerfomence)
   return renderPlainText(statementData, plays)
 
   function enrichPerfomence(aPerformance) {
     const result = Object.assign({}, aPerformance) // 얕은 복사
-    result.play = playFor(result);
+    result.play = playFor(result)
     return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
   }
 }
 
