3a5349e305629db280c0ca0e32f47e753bd75353
강용석 <kangyongsuek@gmail.com>
2022-02-24 20:02:48 +0900

    init: 리팩토링 실습 코드 환경 세팅

---
+++ first-example/first_example.test.js
@@ -0,0 +1,16 @@
+import statement from './index.js';
+import invoice from './data/invoices.json'
+import plays from './data/plays.json'
+
+describe('test', () => {
+  it('aaa', () => {
+    const result = statement(invoice, plays)
+    expect(result).toBe(`청구내역 (고객명: BigCo)
+ Hamlet: $650.00 (55석)
+ As You Like It: $580.00 (35석)
+ Othello: $500.00 (40석)
+총액: $1,730.00
+적립 포인트: 47점
+`)
+  })
+})
