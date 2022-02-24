c8f355273fcb4eeeb67492445d8534a54ff0a217
강용석 <kangyongsuek@gmail.com>
2022-02-24 21:48:02 +0900

    modify: totalAmount 임시 함수로 추출

--- first-example/index.js
+++ first-example/index.js
@@ -58,14 +58,22 @@ function totalVolumeCredits() {
   return volumeCredits
 }
 
-function statement(invoice, plays) {
+function appleSauce() {
   let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf)
+  }
+
+  return totalAmount
+}
+
+function statement(invoice, plays) {
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
   
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf)
   }
+  let totalAmount = appleSauce()
 
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
