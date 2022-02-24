4e249eee36559bfebf3e43e55ede58e2639fd9d3
강용석 <kangyongsuek@gmail.com>
2022-02-24 23:59:46 +0900

    modify: usd 함수 중복 제거

--- first-example/index.js
+++ first-example/index.js
@@ -18,14 +18,6 @@ function renderPlainText(data) {
   result += `총액: ${usd(data.totalAmount)}\n`
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`
   return result
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100)
-  }
 }
 
 
