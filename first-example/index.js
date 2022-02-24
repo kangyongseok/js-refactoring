89b5f35c39d5da9017dc6dce2ef846ea5584e432
강용석 <kangyongsuek@gmail.com>
2022-02-24 21:35:31 +0900

    modify: 문장 슬라이드하기

--- first-example/index.js
+++ first-example/index.js
@@ -51,13 +51,13 @@ function usd(aNumber) {
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
   
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf)
   }
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
