310c32200e33773e567e67d89f0522311a298672
강용석 <kangyongsuek@gmail.com>
2022-02-24 23:40:52 +0900

    modify: 데이터 처리에 해당하는 코드를 모두 별도 함수로 분리

--- first-example/index.js
+++ first-example/index.js
@@ -27,13 +27,14 @@ function renderPlainText(data, plays) {
 
 }
 
-function statement(invoice, plays) {
+function createStatementData(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerfomence)
   statementData.totalAmount = totalAmount(statementData)
   statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-  return renderPlainText(statementData, plays)
+  return statementData
+
 
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
@@ -86,6 +87,10 @@ function statement(invoice, plays) {
 
     return result
   }
+}
+
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays))
 }
 
 module.exports = statement
