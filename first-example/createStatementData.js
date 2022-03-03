63bf7f3424970905fca095a329d26901ec11ce25
강용석 <kangyongsuek@gmail.com>
2022-03-04 00:17:40 +0900

    modify: 적립 포인트 계산함수도 동일하게 이동

--- first-example/createStatementData.js
+++ first-example/createStatementData.js
@@ -5,6 +5,15 @@ class PerformanceCalculator {
     this.play = aPlay
   }
 
+  get volumeCredits() {
+    let result = 0
+    result += Math.max(this.performance.audience - 30, 0)
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performance.audience / 5)
+
+    return result
+  }
+
   get amount() {
     let result = 0
     switch (this.play.type) {
@@ -51,25 +60,12 @@ function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance) // 얕은 복사
     result.play = calculator.play
     result.amount = calculator.amount
-    result.volumeCredits = volumeCreditsFor(result)
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5)
-
-    return result
   }
 }
 module.exports = createStatementData
