0cdd84dabebed2fdddfd88ffbbe97d8dbaf8a889
강용석 <kangyongsuek@gmail.com>
2022-03-04 00:13:35 +0900

    modify: 원본 함수가 이동된 계산작업을 위임하도록 수정

--- first-example/createStatementData.js
+++ first-example/createStatementData.js
@@ -10,16 +10,16 @@ class PerformanceCalculator {
     switch (this.play.type) {
       case 'tragedy':
         result = 40000
-        if (this.aPerformance.audience > 30) {
-          result += 1000 * (this.aPerformance.audience - 30)
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
         }
         break
       case 'comedy':
         result = 30000
-        if (this.aPerformance.audience > 20) {
-          result += 10000 + 500 * (this.aPerformance.audience - 20)
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
         }
-        result += 300 * this.aPerformance.audience
+        result += 300 * this.performance.audience
         break
       default:
         throw new Error(`알 수 없는 장르: ${this.play.type}`)
@@ -60,26 +60,7 @@ function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-    }
-
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
   }
 
   function volumeCreditsFor(aPerformance) {
