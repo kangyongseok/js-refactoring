6043a560a00f3f8cac55fc561d6a53161ba2954b
강용석 <kangyongsuek@gmail.com>
2022-04-27 18:34:53 +0900

    Refactoring: 예제1 함수 인라인 하기 적용

--- 6.2_Inline-Function/index.js
+++ 6.2_Inline-Function/index.js
@@ -2,13 +2,8 @@
 
 // #1
 function getRating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+  return (driver.numberOfLateDeliveries > 5) ? 2 : 1;
 }
-
-function moreThanFiveLateDeliveries(driver) {
-  return driver.numberOfLateDeliveries > 5;
-}
-
 
 // #2
 function reportLines(aCustomer) {
